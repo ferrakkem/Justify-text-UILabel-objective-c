132a3e2f576e00432018796fa164b5ab07156bbb
ferrakkem <ferrakkem@gmail.com>
2017-02-02 12:19:38 +0600

    Initial Commit

---
+++ JustifyText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JustifyText
+//
+//  Created by Ferrakkem Bhuiyan on 2/2/17.
+//  Copyright © 2017 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
